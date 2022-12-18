c6d76a4b9ca5fc88afdcdb47000bd56e780b39cf
Georg Veichtlbauer <georg@vware.at>
2022-12-18 13:29:40 +0100

    TEMP TEMP bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,14 +87,11 @@ BOARD_KERNEL_CMDLINE := \
     console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 \
     video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 \
-    swiotlb=2048 loop.max_part=7
+    swiotlb=2048 loop.max_part=7 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_OFFSET := 0x80000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_SECOND_OFFSET := 0x00000000
-BOARD_TAGS_OFFSET := 0x00000100
+
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
     LLVM=1
